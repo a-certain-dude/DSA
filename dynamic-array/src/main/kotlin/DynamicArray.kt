8476e572e3e63cf11c17339239f153475ab43b88
Jose <nkwantabisajoseph25@gmail.com>
2025-06-29 08:20:52 +0000

    add get and push function

---
+++ dynamic-array/src/main/kotlin/DynamicArray.kt
@@ -0,0 +1,23 @@
+class DynamicArray(
+    private var array: Array<String?> = arrayOfNulls(size = 1),
+    private var capacity: Int = 1,
+    private var length: Int = 0
+) {
+
+    fun get(index: Int): String? = if (index >= 0 && index < length) array[index] else "IndexOutOfBounds"
+
+    /*if the capacity is full it doubles*/
+    fun push(word: String) {
+        if (length == capacity) {
+            val tempArray = arrayOfNulls<String>(size = capacity * 2)
+            for (item in array.indices) {
+                tempArray[item] = array[item]
+            }
+            array = tempArray
+            capacity = capacity * 2 /*🤷‍♀️*/
+        }
+    }
+
+}
+
+
